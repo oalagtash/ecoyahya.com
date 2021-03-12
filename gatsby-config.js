a13e6ef1ebb7f3dd7c62d39052b3c59f0340cee4
ecoyahya <yalagtash@gmail.com>
2021-03-13 01:23:57 +0200

    testing2

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Gatsby Simplefolio`,
-        short_name: `Simplefolio`,
+        short_name: `EcoYahya`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#02aab0`,
