05581631952e931cef29fedd730484791c604bb3
ecoyahya <76744583+ecoyahya@users.noreply.github.com>
2025-08-13 12:15:12 +0300

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -27,7 +27,7 @@ const About = () => {
   return (
     <section id="about">
       <Container>
-        <Title title="About US" />
+        <Title title="About" />
         <Row className="about-wrapper">
           <Col md={6} sm={12}>
             <Fade bottom duration={1000} delay={600} distance="30px">
