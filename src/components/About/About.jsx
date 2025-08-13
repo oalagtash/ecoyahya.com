6bb37e5f112df900fa5a8f2fd6f66277d82b4e00
ecoyahya <76744583+ecoyahya@users.noreply.github.com>
2025-08-13 13:35:47 +0300

    Update About.jsx line 51 fix

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -48,7 +48,7 @@ const About = () => {
                       target="_blank"
                       rel="noopener noreferrer"
                       className="cta-btn cta-btn--resume"
-                      href={Contact us}
+                      href={"#contact"}
                     >
                       Resume
                     </a>
